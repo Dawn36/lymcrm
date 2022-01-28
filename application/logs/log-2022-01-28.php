f3ee958eeadcb66a363660818f069f4d4e73bb65
Asad Bhatti <82872024+bhattiasad12@users.noreply.github.com>
2022-01-28 22:33:18 +0500

    asad txt added

---
+++ application/logs/log-2022-01-28.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2022-01-28 17:23:33 --> UTF-8 Support Enabled
+DEBUG - 2022-01-28 17:23:33 --> No URI present. Default controller set.
+DEBUG - 2022-01-28 17:23:33 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2022-01-28 17:23:33 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'lymcrm' C:\xampp\htdocs\lymcrm\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-01-28 17:23:33 --> Unable to connect to the database
+DEBUG - 2022-01-28 17:23:34 --> UTF-8 Support Enabled
+DEBUG - 2022-01-28 17:23:34 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2022-01-28 17:23:34 --> 404 Page Not Found: Faviconico/index
