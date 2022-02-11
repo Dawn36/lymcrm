14e47e6e5e26aae4e9aa52a362d8e75d55295c60
dawn <dawngill08@gmail.com>
2022-02-12 01:17:57 +0500

    Update email_self_history.php

--- application/views/email_self_history.php
+++ application/views/email_self_history.php
@@ -7,29 +7,29 @@ $heading = "Email History";
                 <div class="col-lg-12 ">
                     <div id="panel-1" class="panel">
                         <div class="panel-container show">
-                            <div class="panel-content">
+                            <div class="panel-content" style="overflow: auto;">
                                 <table id="datatable_tabletools12" class="table table-bordered table-hover table-striped w-100">
                                     <thead class="bg-primary-600 bg-brand-gradient">
                                         <tr>
-                                            <th nowrap>
+                                            <th >
                                                 <center>Owner Name</center>
                                             </th>
-                                            <th nowrap>
+                                            <th >
                                                 <center>Owner Email</center>
                                             </th>
-                                            <th nowrap>
+                                            <th >
                                                 <center>Installment</center>
                                             </th>
-                                            <th nowrap>
+                                            <th >
                                                 <center>Building Name</center>
                                             </th>
-                                            <th nowrap>
+                                            <th >
                                                 <center>Apartment No</center>
                                             </th>
-                                            <th nowrap>
+                                            <th >
                                                 <center>Send by</center>
                                             </th>
-                                             <th nowrap>
+                                             <th >
                                                 <center>Send At</center>
                                             </th>
                                         </tr>
@@ -84,89 +84,93 @@ $heading = "Email History";
 
 
 <script type="text/javascript">
-    $(document).ready(function() {
+    // DataTable();
+    // function DataTable() {
+    //     $('#datatable_tabletools12').dataTable({
+    //          "language": {
+    //                 "processing": '<i class="fa fa-spinner fa-spin" style="font-size:24px;color:rgb(75, 183, 245);"></i>'
+    //              }
+    //         responsive: false,
+    //         lengthChange: false,
+    //         scrollX: true,
+    //         dom:
+    //             /*  --- Layout Structure 
+    //                 --- Options
+    //                 l   -   length changing input control
+    //                 f   -   filtering input
+    //                 t   -   The table!
+    //                 i   -   Table information summary
+    //                 p   -   pagination control
+    //                 r   -   processing display element
+    //                 B   -   buttons
+    //                 R   -   ColReorder
+    //                 S   -   Select
 
-        $('#datatable_tabletools12').dataTable({
-            responsive: true,
-            lengthChange: false,
-            dom:
-                /*  --- Layout Structure 
-                    --- Options
-                    l   -   length changing input control
-                    f   -   filtering input
-                    t   -   The table!
-                    i   -   Table information summary
-                    p   -   pagination control
-                    r   -   processing display element
-                    B   -   buttons
-                    R   -   ColReorder
-                    S   -   Select
+    //                 --- Markup
+    //                 < and >             - div element
+    //                 <"class" and >      - div with a class
+    //                 <"#id" and >        - div with an ID
+    //                 <"#id.class" and >  - div with an ID and a class
 
-                    --- Markup
-                    < and >             - div element
-                    <"class" and >      - div with a class
-                    <"#id" and >        - div with an ID
-                    <"#id.class" and >  - div with an ID and a class
-
-                    --- Further reading
-                    https://datatables.net/reference/option/dom
-                    --------------------------------------
-                 */
-                "<'row mb-3'<'col-sm-12 col-md-6 d-flex align-items-center justify-content-start'f><'col-sm-12 col-md-6 d-flex align-items-center justify-content-end'lB>>" +
-                "<'row'<'col-sm-12'tr>>" +
-                "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
-            buttons: [
-                /*{
-                    extend:    'colvis',
-                    text:      'Column Visibility',
-                    titleAttr: 'Col visibility',
-                    className: 'mr-sm-3'
-                },*/
-                {
-                    extend: 'pdfHtml5',
-                    title: '<?php echo $heading; ?>',
-                    text: 'PDF',
-                    titleAttr: 'Generate PDF',
-                    className: 'btn-outline-danger btn-sm mr-1'
-                },
-                {
-                    extend: 'excelHtml5',
-                    title: '<?php echo $heading; ?>',
-                    text: 'Excel',
-                    titleAttr: 'Generate Excel',
-                    className: 'btn-outline-success btn-sm mr-1'
-                },
-                {
-                    extend: 'csvHtml5',
-                    title: '<?= $heading; ?>',
-                    text: 'CSV',
-                    titleAttr: 'Generate CSV',
-                    className: 'btn-outline-primary btn-sm mr-1'
-                },
-                {
-                    extend: 'copyHtml5',
-                    title: '<?php echo $heading; ?>',
-                    text: 'Copy',
-                    titleAttr: 'Copy to clipboard',
-                    className: 'btn-outline-primary btn-sm mr-1'
-                },
-                {
-                    extend: 'print',
-                    text: 'Print',
-                    titleAttr: 'Print Table',
-                    title: '<?php echo $heading; ?>',
-                    customize: function(win) {
-                        $(win.document.body).find('h1').css('text-align', 'center');
-                        $(win.document.body).css('font-size', '9px');
-                        $(win.document.body).find('table')
-                            .addClass('compact')
-                            .css('font-size', 'inherit');
-                    },
-                    className: 'btn-outline-primary btn-sm'
-                }
-            ]
-        });
-    });
+    //                 --- Further reading
+    //                 https://datatables.net/reference/option/dom
+    //                 --------------------------------------
+    //              */
+    //             "<'row mb-3'<'col-sm-12 col-md-6 d-flex align-items-center justify-content-start'f><'col-sm-12 col-md-6 d-flex align-items-center justify-content-end'lB>>" +
+    //             "<'row'<'col-sm-12'tr>>" +
+    //             "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+    //         buttons: [
+    //             {
+    //                 extend:    'colvis',
+    //                 text:      'Column Visibility',
+    //                 titleAttr: 'Col visibility',
+    //                 className: 'mr-sm-3'
+    //             },
+    //             {
+    //                 extend: 'pdfHtml5',
+    //                 title: '<?php echo $heading; ?>',
+    //                 text: 'PDF',
+    //                 titleAttr: 'Generate PDF',
+    //                 className: 'btn-outline-danger btn-sm mr-1'
+    //             },
+    //             {
+    //                 extend: 'excelHtml5',
+    //                 title: '<?php echo $heading; ?>',
+    //                 text: 'Excel',
+    //                 titleAttr: 'Generate Excel',
+    //                 className: 'btn-outline-success btn-sm mr-1'
+    //             },
+    //             {
+    //                 extend: 'csvHtml5',
+    //                 title: '<?= $heading; ?>',
+    //                 text: 'CSV',
+    //                 titleAttr: 'Generate CSV',
+    //                 className: 'btn-outline-primary btn-sm mr-1'
+    //             },
+    //             {
+    //                 extend: 'copyHtml5',
+    //                 title: '<?php echo $heading; ?>',
+    //                 text: 'Copy',
+    //                 titleAttr: 'Copy to clipboard',
+    //                 className: 'btn-outline-primary btn-sm mr-1'
+    //             },
+    //             {
+    //                 extend: 'print',
+    //                 text: 'Print',
+    //                 titleAttr: 'Print Table',
+    //                 title: '<?php echo $heading; ?>',
+    //                 customize: function(win) {
+    //                     $(win.document.body).find('h1').css('text-align', 'center');
+    //                     $(win.document.body).css('font-size', '9px');
+    //                     $(win.document.body).find('table')
+    //                         .addClass('compact')
+    //                         .css('font-size', 'inherit');
+    //                 },
+    //                 className: 'btn-outline-primary btn-sm'
+    //             }
+    //         ]
+    //     });
+    // }
 
   
   
